ca5de2466879ab126472b00126df9f80cd5ea142
jawad ilyas <mughaljawad12@gmail.com>
2024-08-18 02:19:30 +0500

    header section is aslo complete

--- src/Components/pagination/Pagination.jsx
+++ src/Components/pagination/Pagination.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { RoundedButtonHover ,  SquareButton } from "../index"
 function Pagination() {
   return (
-    <div className='w-full flex flex-row justify-between'>
+    <div className='w-full flex flex-row justify-between items-center'>
           <RoundedButtonHover label='Back' textColor='EeieBlack' paddingX="px-6" paddingY='pt-1 pb-2' textSize="text-md" / >
-          <div className='space-x-4'>
+          <div className='lg:space-x-4 space-x-1 '>
               <SquareButton label='1' textColor='EeieBlack' />
               <SquareButton label='2' textColor='EeieBlack' />
               <SquareButton label='3' textColor='EeieBlack' />
-              <SquareButton label='4' textColor='EeieBlack' />
-              <SquareButton label='5' textColor='EeieBlack' />
+         
           </div>
           <RoundedButtonHover label='Next' textColor='EeieBlack' paddingX="px-6" paddingY='pt-1 pb-2' textSize="text-md" / >
     </div>
