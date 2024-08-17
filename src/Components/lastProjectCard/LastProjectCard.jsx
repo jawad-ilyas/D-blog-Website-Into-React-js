ecc04b91c574ed6af45541db1e7fcfe3309d0d78
jawad ilyas <mughaljawad12@gmail.com>
2024-08-18 02:20:47 +0500

    header section is aslo complete

--- src/Components/lastProjectCard/LastProjectCard.jsx
+++ src/Components/lastProjectCard/LastProjectCard.jsx
@@ -4,13 +4,13 @@ import { RoundedButtonHoverLink } from "../index"
 function LastProjectCard({ title, description, slug, timeDuration }) {
     return (
         <div className='px-8 space-y-4'>
-            <h1 className='text-wrap text-EeieBlack  text-xl font-normal cursor-pointer'>
+            <h1 className='text-wrap pt-2 text-EeieBlack  text-xl font-normal cursor-pointer'>
                 {title}
             </h1>
             <time className='text-Silver me-2'>
                 {timeDuration}
             </time>
-            <p className='text-Gray  text-xl pb-4 font-light '>{description}</p>
+            <p className='text-Gray text-base  lg:text-xl pb-4 font-light '>{description}</p>
      
             <RoundedButtonHoverLink className="mt-4" label="Watch Now" textColor="black" paddingX="px-4" paddingY="pb-2 pt-2" textSize='text-lg'/>
         </div>
